7b5089d0955e55a05cc028403e5a71deab37fb69
Dmitry Ganotskiy <dmitryganotskiy@gmail.com>
2023-12-10 15:34:01 +0100

    printGrid

--- grid.py
+++ grid.py
@@ -4,3 +4,9 @@ class Grid:
         self.cols = 10
         self.size = 30
         self.grid=[[0 for j in range(self.cols)] for i in range(self.rows)]
+
+    def PrintGrid(self):
+        for row in range(self.rows):
+            for column in range(self.cols):
+                print(self.grif[row][column], end=" ")
+            print()
